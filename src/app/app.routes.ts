8643b5610f05cc97373a3feec634a4c7cd6f1c06
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2025-05-14 23:06:40 -0500

    arreglo barra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { CategoriasComponent } from './pages/categorias/categorias.component';
 import { GestionReportesComponent } from './pages/reportes/gestion-reportes/gestion-reportes.component';
 import { DetalleReporteComponent } from './pages/reportes/detalle-reporte/detalle-reporte.component';
 import { CrearReporteComponent } from './pages/reportes/crear-reporte/crear-reporte.component';
+import { CodigoConfirmacionComponent } from './pages/codigo-confirmacion/codigo-confirmacion.component';
 
 
 
@@ -22,5 +23,6 @@ export const routes: Routes = [
    { path: 'gestion-reportes', component: GestionReportesComponent },
    { path: 'detalle-reporte/:id', component: DetalleReporteComponent },
    { path: 'editar-reporte/:id', component: CrearReporteComponent },
-   { path: 'crear-reporte', component: CrearReporteComponent}
+   { path: 'crear-reporte', component: CrearReporteComponent},
+   { path: 'codigo-confirmacion', component: CodigoConfirmacionComponent   }
 ];
