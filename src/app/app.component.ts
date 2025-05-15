75db4890ea40a0d91c6ce9464fb906d59bd6911f
Joseph Yulian Garcia <josephy.garciac@uqvirtual.edu.co>
2025-05-14 21:17:44 -0500

    Cambios Master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
